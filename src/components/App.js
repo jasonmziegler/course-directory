635cee270c3c032bb1b3419a381dd56a7229450e
jasonmziegler <jason@insightfulautomation.com>
2018-12-14 16:27:57 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  BrowserRouter,
+  Route 
+} from 'react-router-dom';
+
+const App = () => (
+  <div className="container">
+
+  </div>
+);
+
+export default App;
