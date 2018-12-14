360ad53be2a04f09add6bcd4ae1d513bab03de3a
jasonmziegler <jason@insightfulautomation.com>
2018-12-14 16:54:59 -0600

    update app.js to include basic routes

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,23 @@ import {
   Route 
 } from 'react-router-dom';
 
-const App = () => (
-  <div className="container">
+// App components
+import Header from './Header';
+import Home from './Home';
+import About from './About';
+import Teachers from './Teachers';
+import Courses from './Courses';
 
-  </div>
+const App = () => (
+  <BrowserRouter>
+    <div className="container">
+      <Header />
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" render={() => <About title='About'/>} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
+    </div>
+  </BrowserRouter>
 );
 
 export default App;
