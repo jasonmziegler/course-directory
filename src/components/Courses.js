13af53c3aa73f27011d9492e559b06b75a4738ec
jasonmziegler <jason@insightfulautomation.com>
2018-12-14 23:20:03 -0600

    Add navigation using routes

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import {
+  Route, 
+  NavLink,
+  Redirect,
+  Switch } from 'react-router-dom';
 
-const Courses = () => (
+import CourseContainer from './courses/CourseContainer';
+import { HTMLCourses, CSSCourses, JSCourses  } from '../data/courses';
+import NotFound from './NotFound';
+
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to={`${match.url}/html`}>HTML</NavLink></li>
+        <li><NavLink to={`${match.url}/css`}>CSS</NavLink></li>
+        <li><NavLink to={`${match.url}/javascript`}>JavaScript</NavLink></li>
       </ul>
     </div>
     
     {/* Write routes here... */}
+    <Switch>
+      <Route exact path={match.path} render={() => <Redirect to={`${match.path}/html`} />} />
+      <Route path={`${match.path}/html`} render={ () => <CourseContainer data={HTMLCourses}/> } />
+      <Route path={`${match.path}/css`} render={ () => <CourseContainer data={CSSCourses}/> } />
+      <Route path={`${match.path}/javascript`} render={ () => <CourseContainer data={JSCourses}/> } />
+      <Route component={NotFound} />
+    </Switch>
+
   </div>
 );
 
